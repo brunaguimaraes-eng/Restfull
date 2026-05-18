9774461fcff3ca5174fffc948239e7bdf7663752
Bruna Guimaraes <bruna.guimaraes@saipos.com>
2026-05-17 21:56:53 -0300

    NJS65 - WIP

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+let server = http.createServer((req, res) => {
+
+    console.log('URL', req.url);
+    console.log('METHOD', req.method);
+
+    switch (req.url) {
+
+        case'/':
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html'):
+            res.end(<h1>Olá</h1>);
+
+            break;
+
+    }
+});
+
+server.listen(3000, '127.0.0.1', () => {
+
+    console.log('serv ok');
+
+})
