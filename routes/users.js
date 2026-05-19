1c0ec21183c0d1b5e62e758ae98e063cc2ffa3eb
Bruna Guimaraes <bruna.guimaraes@saipos.com>
2026-05-18 23:03:44 -0300

    NJS72 - listando usuários no db

--- routes/users.js
+++ routes/users.js
@@ -6,39 +6,48 @@ let db = new NeDB({
 
 })
 
-module.exports = (app) => {
+module.exports = app => {
 
-    app.get('/users', (req, res) => { //define uma rota para buscar o usuário
+        app.get('/users', (req, res) => { //define uma rota para buscar o usuário
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json'); //indica que o conteúdo é um json, em seguida uma lista de array
-        res.json({
-            users: [{
-             name:'Bru',
-                email:'bruna@saipos.com',
-                id:1
-            }]
+            db.find({}).sort({name:1}).exec((err, users) => {
+
+                if(err){
+
+                    console.log(`error: ${err}`);
+                    res.status(400).json({
+                        error:err
+                    });
+                }else{
+
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json'); //indica que o conteúdo é um json, em seguida uma lista de array
+                    res.json({
+                        users
+                    });
+
+                }
+            });
+
+        });   
+
+        app.post('/users', (req, res) => { //define uma rota para criar e/ou enviar dados de um novo usuário
+
+            db.insert(req.body, (err, users) => {
+
+                if(err){
+                    console.log(`error: ${err}`);
+                    res.status(400).json({
+                        error: err
+                    })
+                }else{
+
+                    res.status(200).json(users);
+                }
+
+            });
+
         });
 
-    });
-
-    app.post('/users', (req, res) => { //define uma rota para criar e/ou enviar dados de um novo usuário
-
-        db.insert(req.body, (err, user) => {
-
-            if(err){
-                console.log(`error: ${err}`);
-                res.status(400).json({
-                    error: err
-                })
-            }else{
-
-                res.status(200).json(user);
-            }
-
-        });
-
-    });
-
-};
+    };
 
