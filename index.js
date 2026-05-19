f0d319d6d5134ff5538942ea9276a660f6b10674
Bruna Guimaraes <bruna.guimaraes@saipos.com>
2026-05-18 21:57:31 -0300

    NJS70 - Postman

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 consign().include('routes').into(app);
 
@@ -9,4 +13,4 @@ app.listen(3000, '127.0.0.1', () => {
 
     console.log('serv ok');
 
-})
+});
